06b46ee98dc873389e90736e842b02471025c408
tanghao <363310279@qq.com>
2018-05-15 15:20:13 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+ <div class="title">热销推荐</div>
+ <ul>
+    <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img  class="item-img" :src='item.imgUrl' />
+       <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+        <button class="item-button">查看详情</button>
+       </div>
+    </li>
+ </ul>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
+        title: '北京森林动物园',
+        desc: '超级无敌好玩的动物园，主题公园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
+        title: '北京森林动物园',
+        desc: '超级无敌好玩的动物园，主题公园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
+        title: '北京森林动物园',
+        desc: '超级无敌好玩的动物园，主题公园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top: .2rem
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+   flex: 1
+   padding: .1rem
+   min-width: 0
+   .item-title
+     line-height: .54rem
+     font-size: .32rem
+     ellipsis()
+   .item-desc
+     line-height: .4rem
+     color: #ccc
+     ellipsis()
+   .item-button
+     line-height: .44rem
+     margin-top: .16rem
+     background: #ff9300
+     padding: 0 0.2rem
+     border-radius: .06rem
+     color: #fff
+</style>
