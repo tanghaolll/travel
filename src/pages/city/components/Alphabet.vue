734459491cdf48b8179ed9f9dcb2bb4718503d4d
tanghao <363310279@qq.com>
2018-05-17 10:43:15 +0800

    search finshed

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -47,8 +47,7 @@ export default {
           if (index >= 0 && index < this.letters.length) {
             this.$emit('change', this.letters[index])
           }
-        },16)
-       
+        }, 16)
       }
     },
     handleTouchEnd () {
