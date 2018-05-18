e3aff213649a3475898731612be98b3acf882890
tanghao <363310279@qq.com>
2018-05-18 17:08:40 +0800

    banner finsh

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+      <div class="banner" @click="handleBannerClick">
+        <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1701/92/9239dcf4c58977eba3.water.jpg_600x330_c56a7d8d.jpg" />
+      <div class="banner-info">
+            <div class="banner-title">亚龙湾瑞德姆海底演艺主题餐厅</div>
+            <div class="banner-number"><span class="iconfont banner-icon">&#xe626;</span>4</div>
+      </div>
+      </div>
+      <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1701/92/9239dcf4c58977eba3.water.jpg_r_800x800_aa4e94c4.jpg', 'http://img1.qunarzz.com/sight/p0/1701/bf/bf994dc3697a74eea3.water.jpg_r_800x800_635d9d03.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display:flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        line-height: .32rem
+        height: .32rem
+        padding: 0 .4rem
+        margin-top: .14rem
+        border-radius: .2rem
+        background: rgba(0,0,0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
